01619ce810d15dd6d3219e516be0543d6c4a6f2d
wildeflow <crossmail@gmx.de>
2025-06-19 12:29:00 +0200

    add db schema

---
+++ schemas/000-complete.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS artists;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS artists (
+    id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    link TEXT,
+    picture TEXT,
+    picture_small TEXT,
+    picture_medium TEXT,
+    picture_big TEXT,
+    picture_xl TEXT,
+    nb_album INTEGER,
+    nb_fan INTEGER,
+    radio BOOLEAN,
+    tracklist TEXT,
+    type TEXT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS albums (
+    id INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    link TEXT,
+    cover TEXT,
+    cover_small TEXT,
+    cover_medium TEXT,
+    cover_big TEXT,
+    cover_xl TEXT,
+    genre_id INTEGER,
+    fans INTEGER,
+    release_date DATE,
+    record_type TEXT,
+    explicit_lyrics BOOLEAN,
+    artist_id INTEGER NOT NULL,
+    PRIMARY KEY (id)
+);
