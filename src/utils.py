960cc23c91b06194363806b9fc429c1493dfbcb0
wildeflow <crossmail@gmx.de>
2025-06-23 12:11:13 +0200

    add utils module

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import os
+
+from config import BLACKLIST
+
+
+def fast_scandir(dirname: str, blacklist: set | None = None) -> list[str]:
+    """Fast directory scanner to retrieve all subdirectories."""
+    if blacklist is None:
+        blacklist = BLACKLIST
+    subfolders = [
+        f.path
+        for f in os.scandir(dirname)
+        if f.is_dir() and f.name not in blacklist
+    ]
+    for dir_name in list(subfolders):
+        subfolders.extend(fast_scandir(dir_name))
+    return subfolders
